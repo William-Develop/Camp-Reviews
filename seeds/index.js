3bcc9f5fcb877fb544c0098409e8e3efd63e9174
William-Maldonado <willpaiolo@live.com>
2023-11-15 12:43:51 -0600

    add seeds directory with cities.js seedHelpers.js index.js

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+const mongoose = require ("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+    console.log("Database connected!");
+});
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    let camp = new Campground({tittle: "purple field"});
+    await camp.save();
+}
