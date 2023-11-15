609aa379d4074ffa1e5d000783ac760c127e0dac
William-Maldonado <willpaiolo@live.com>
2023-11-15 14:11:11 -0600

    Create basic seed setup to feed the data base when needed

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,7 @@
 const mongoose = require ("mongoose");
-const Campground = require("./models/campground");
+const cities = require("./cities");
+const {places, descriptors} = require("./seedHelpers");
+const Campground = require("../models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
 });
@@ -10,8 +12,20 @@ db.once("open", () => {
     console.log("Database connected!");
 });
 
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
     await Campground.deleteMany({});
-    let camp = new Campground({tittle: "purple field"});
-    await camp.save();
+    for(let i = 0; i < 50 ; i ++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            tittle: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
 }
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
