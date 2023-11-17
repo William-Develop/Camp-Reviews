caf6bc04c96f6aa0152c66a52b76f78c100b61e0
William-Maldonado <willpaiolo@live.com>
2023-11-16 18:17:59 -0600

    add route to edit new campgrounds

--- app.js
+++ app.js
@@ -15,24 +15,35 @@ db.once("open", () => {
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     res.render("home")
-})
+});
 
 app.get("/campgrounds", async (req, res) => {
     const campgrounds =  await Campground.find({});
     res.render("campgrounds/index", {campgrounds})
-})
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/camprounds/${campground._id}`)
+});
 
 app.get("/campgrounds/:id", async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render("campgrounds/show", { campground });
-})
-
+});
 
 
 app.listen(3000, () => {
     console.log("Server started on port: 3000")
-})
+});
