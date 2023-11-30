589a706ea139587c1b40d7dbe6240f0f5288839b
William-Maldonado <willpaiolo@live.com>
2023-11-30 14:24:42 -0600

    add descriptive comments to models/review.js and seeds/index.js

--- models/review.js
+++ models/review.js
@@ -1,9 +1,14 @@
+// Import mongoose module, provides a straight-forward, schema-based solution to model the application data
 const mongoose = require ("mongoose");
+
+// Destructuring Schema from mongoose
 const Schema = mongoose.Schema;
 
+// Creating a new Schema for reviews. Each review will have a body (text) and a rating (number)
 const reviewSchema = new Schema({
     body: String,
     rating: Number,
 });
 
+// Exporting the Review model, which can be used in other parts of our application
 module.exports = mongoose.model("Review", reviewSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,17 @@
 // Import necessary modules and data
-const mongoose = require ("mongoose");
-const cities = require("./cities");
-const {places, descriptors} = require("./seedHelpers");
-const Campground = require("../models/campground");
+const mongoose = require ("mongoose"); // Mongoose for MongoDB interactions
+const cities = require("./cities"); // Data for cities
+const {places, descriptors} = require("./seedHelpers"); // Helper data for seeding
+const Campground = require("../models/campground"); // Campground model
 
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {});
 
 // Set up connection to MongoDB
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
+db.on("error", console.error.bind(console, "connection error: ")); // Log any connection errors
 db.once("open", () => {
-    console.log("Database connected!");
+    console.log("Database connected!"); // Log successful connection
 });
 
 // Function to return a random element from an array
@@ -25,20 +25,20 @@ const seedDB = async () => {
     for(let i = 0; i < 50 ; i ++) {
         // Generate a random index for cities array
         const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
+        const price = Math.floor(Math.random() * 20) + 10; // Generate a random price for the campground
         // New instance campground of model saved to the database
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: "https://source.unsplash.com/collection/483251",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea inventore sit, eveniet harum, voluptates recusandae ducimus aliquid error quasi amet natus esse cum, cupiditate non a ullam unde. Provident, nihil.",
-            price
+            location: `${cities[random1000].city}, ${cities[random1000].state}`, // Set location to a random city
+            title: `${sample(descriptors)} ${sample(places)}`, // Generate a random title
+            image: "https://source.unsplash.com/collection/483251", // Set image URL
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea inventore sit, eveniet harum, voluptates recusandae ducimus aliquid error quasi amet natus esse cum, cupiditate non a ullam unde. Provident, nihil.", // Set description
+            price // Set price
         })
-        await camp.save();
+        await camp.save(); // Save the new campground to the database
     }
 }
 
 // Call seedDB and close the connection to MongoDB
 seedDB().then(() => {
-    mongoose.connection.close();
+    mongoose.connection.close(); // Close the connection after seeding
 })
