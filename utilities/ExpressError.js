5c66bb63b1ceb4113e02d6c07d2705249b9df4db
William-Maldonado <willpaiolo@live.com>
2023-11-30 14:55:59 -0600

    add descriptive comments to utilities/ExpressError.js utilities/catchAsync.js views/campgrounds/edit.ejs views/campgrounds/index.ejs views/campgrounds/new.ejs views/campgrounds/show.ejs

--- utilities/ExpressError.js
+++ utilities/ExpressError.js
@@ -1,9 +1,12 @@
+// Define a custom error class that extends the built-in Error class
 class ExpressError extends Error {
+    // The constructor accepts two parameters: a message and a status code
     constructor(message, statusCode) {
-        super();
-        this.message = message;
-        this.statusCode = statusCode;
+        super(); // Call the parent class's constructor
+        this.message = message; // Set the error message
+        this.statusCode = statusCode; // Set the HTTP status code
     }
 }
 
+// Export the custom error class for use in other parts of the application
 module.exports = ExpressError;
--- utilities/catchAsync.js
+++ utilities/catchAsync.js
@@ -1,6 +1,10 @@
-// Function wraps another Express middleware to ensure asynchronous errors are forwarded to the next() function for handling.
+// This module exports a function that takes another function (func) as an argument
 module.exports = func => {
+    // It returns a new function that takes three arguments: req, res, and next
     return(req, res, next) => {
+        // It calls the original function (func) with req, res, and next as arguments
+        // If the original function returns a Promise that rejects, it calls next with the reason for rejection
+        // This is useful for passing errors to error-handling middleware in Express.js
         func(req, res, next).catch(next);
     }
 }
