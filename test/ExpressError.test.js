0cb342613d02021549da1dc53a1c48090cf4841a
✨ William Maldonado ✨ <willpaiolo@live.com>
2023-12-14 14:47:23 -0600

    add descriptive comments to ExpressError.test.js

--- test/ExpressError.test.js
+++ test/ExpressError.test.js
@@ -1,15 +1,19 @@
+// Import necessary functions from vitest testing library
 import { expect, test } from "vitest";
+
+// Import ExpressError class from utilities directory
 import ExpressError from "../utilities/ExpressError";
 
+// Define a new test for the ExpressError class
 test("ExpressError", () => {
-    // Arrange
+    // Arrange: Set up the data for the test. Message and status code.
     const message = "Test Error";
     const statusCode = 400;
 
-    // Act
+    // Act: Perform the action to be tested. Create a new ExpressError.
     const error = new ExpressError(message, statusCode)
 
-    // Assert
+    // Assert: Check that the results are as expected. Error's message and status code should match the input.
     expect(error.message).toBe(message)
     expect(error.statusCode).toBe(statusCode)
 })
